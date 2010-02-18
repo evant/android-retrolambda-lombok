e6724afe7e5045ff71d213c99c66218280e5cd3d
Roel Spilker <r.spilker@gmail.com>
2010-02-18 23:58:03 +0100

    Added test for multiple field declarations on one line

---
+++ test/idempotency/G003_MultipleFieldDeclarations.java
@@ -0,0 +1,8 @@
+class G003_MultipleFieldDeclaration {
+    private int x = 0, y;
+    
+    int z, k[] = {};
+    
+    @SuppressWarnings("all")
+    public final String[] name = null, names[] = {{}};
+}
