6fa9d48c7f809d324657a8a4769f707d8d7c504f
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-03-03 22:50:01 +0100

    Added checks for initializers (no return statements, must finish normally), return statements (expressioned or not must match return type being void or not/constructor), constructors (particularly alternative/superConstructorInvocations).

--- src/main/lombok/ast/syntaxChecks/StructuralChecks.java
+++ src/main/lombok/ast/syntaxChecks/StructuralChecks.java
@@ -2,11 +2,20 @@ package lombok.ast.syntaxChecks;
 
 import java.util.List;
 
+import lombok.ast.AlternateConstructorInvocation;
+import lombok.ast.Block;
+import lombok.ast.ConstructorDeclaration;
+import lombok.ast.InstanceInitializer;
 import lombok.ast.MethodDeclaration;
 import lombok.ast.Modifiers;
 import lombok.ast.Node;
+import lombok.ast.Return;
+import lombok.ast.StaticInitializer;
+import lombok.ast.SuperConstructorInvocation;
 import lombok.ast.SyntaxProblem;
+import lombok.ast.Throw;
 import lombok.ast.TypeDeclaration;
+import lombok.ast.TypeReference;
 import lombok.ast.template.SyntaxCheck;
 
 @SyntaxCheck
@@ -26,6 +35,98 @@ public class StructuralChecks {
 			if (!(rawModifiersOfParent instanceof Modifiers)) return;
 			if (((Modifiers)rawModifiers).isAbstract()) return;
 			problems.add(new SyntaxProblem(md, "Abstract methods are only allowed in interfaces and abstract classes"));
+		}
+	}
+	
+	public void initializersMustNotContainReturn(Return node) {
+		Node parent = node;
+		while (parent != null) {
+			if (parent instanceof MethodDeclaration) return;
+			if (parent instanceof StaticInitializer || parent instanceof InstanceInitializer) {
+				problems.add(new SyntaxProblem(node, "Initializers cannot contain return statements."));
+			}
+			parent = parent.getParent();
+		}
+	}
+	
+	public void initializersMustNotContainThrowsDirectlyStatic(StaticInitializer node) {
+		initializersMustNotContainThrowsDirectly(node.getRawBody());
+	}
+	
+	public void initializersMustNotContainThrowsDirectlyInstance(InstanceInitializer node) {
+		initializersMustNotContainThrowsDirectly(node.getRawBody());
+	}
+	
+	private void initializersMustNotContainThrowsDirectly(Node rawBlock) {
+		if (!(rawBlock instanceof Block)) return;
+		for (Node s : ((Block)rawBlock).contents().getRawContents()) {
+			if (s instanceof Throw) {
+				problems.add(new SyntaxProblem(s, "Initializers must complete normally."));
+			}
+		}
+	}
+	
+	public void returnValueVsVoidReturnType(Return node) {
+		boolean shouldBeVoid = node.getRawValue() == null;
+		Node parent = node;
+		while (parent != null) {
+			if (parent instanceof ConstructorDeclaration) {
+				if (!shouldBeVoid) {
+					problems.add(new SyntaxProblem(node, "Constructors cannot return a value."));
+				}
+				return;
+			}
+			if (parent instanceof MethodDeclaration) {
+				Node rawTR = ((MethodDeclaration)parent).getRawReturnTypeReference();
+				if (rawTR instanceof TypeReference) {
+					if (((TypeReference)rawTR).isVoid()) {
+						if (!shouldBeVoid) {
+							problems.add(new SyntaxProblem(node, "void methods cannot return a value."));
+						}
+					} else {
+						if (shouldBeVoid) {
+							problems.add(new SyntaxProblem(node, "method should return a value."));
+						}
+					}
+				}
+				return;
+			}
+		}
+	}
+	
+	public void constructorInvocationsOnlyInConstructorsSuper(SuperConstructorInvocation node) {
+		constructorInvocationsOnlyInConstructors(node);
+	}
+	
+	public void constructorInvocationsOnlyInConstructorsThis(AlternateConstructorInvocation node) {
+		constructorInvocationsOnlyInConstructors(node);
+	}
+	
+	private void constructorInvocationsOnlyInConstructors(Node node) {
+		Node parent = node;
+		while (parent != null) {
+			if (parent instanceof ConstructorDeclaration) return;
+			if (parent instanceof MethodDeclaration) {
+				problems.add(new SyntaxProblem(node, "Calling another constructor directly is only allowed inside other constructors."));
+				return;
+			}
+		}
+	}
+	
+	public void superConstructorInvocationMustBeFirst(SuperConstructorInvocation node) {
+		Node parent = node;
+		while (parent != null) {
+			if (parent instanceof ConstructorDeclaration) {
+				Node rawBlock = ((ConstructorDeclaration)parent).getRawBody();
+				if (rawBlock instanceof Block) {
+					Node n = ((Block)rawBlock).contents().rawFirst();
+					if (n != node) {
+						problems.add(new SyntaxProblem(node,
+								"Calling super must be the first statement in a constructor."));
+					}
+				}
+				return;
+			}
 		}
 	}
 }
