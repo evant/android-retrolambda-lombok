464690d5d55e5656e8267865bd7b747ebdd82b09
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-03-05 13:08:59 +0100

    Moved BasicStructure2 into its proper hierarchy and fleshed out the test with inner classes and interfaces.

--- test/idempotency/BasicStructure2Interface.java
+++
@@ -1,8 +0,0 @@
-public interface BasicStructure2Interface extends java.util.RandomAccess, java.io.Serializable {
-    int field = 0;
-    
-    void x();
-}
-
-interface BasicStructure2InterfaceX {
-}
---
+++ test/idempotency/H001_Interfaces.java
@@ -0,0 +1,18 @@
+public interface H001_Interfaces extends java.util.RandomAccess, java.io.Serializable {
+    int field = 0;
+    
+    void x();
+}
+
+interface H001_Interfaces2 {
+}
+
+interface H001_Interfaces3 {
+    interface InnerInterface {
+        static interface innerInnerInterface {
+        }
+    }
+    
+    class InnerClass {
+    }
+}
