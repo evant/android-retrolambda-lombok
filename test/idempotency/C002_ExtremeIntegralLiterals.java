0805fe20bd298ecac7a9fc2368bb12011fa6252c
Roel Spilker <r.spilker@gmail.com>
2010-03-08 02:29:14 +0100

    Added a test with the extremes of integral literals (0x8000 lots of zeroes, and such).

---
+++ test/idempotency/C002_ExtremeIntegralLiterals.java
@@ -0,0 +1,16 @@
+class C002_ExtremeIntegralLiterals {
+    {
+        int minHexInt = 0x80000000;
+        int minOctInt = 020000000000;
+        int maxHexInt = 0x7fffffff;
+        int maxOctInt = 017777777777;
+        int minusOneHexInt = 0xffffffff;
+        int minusOneOctInt = 037777777777;
+        long minHexLong = 0x8000000000000000L;
+        long minOctLong = 01000000000000000000000L;
+        long maxHexLong = 0x7fffffffffffffffL;
+        long maxOctLong = 0777777777777777777777L;
+        long minusOneHexLong = 0xffffffffffffffffL;
+        long minusOneOctLong = 01777777777777777777777L;
+    }
+}
