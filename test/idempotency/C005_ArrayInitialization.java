d48b0d92ff7815450e7d1f1085ea879aee2806ce
Roel Spilker <r.spilker@gmail.com>
2010-03-13 04:01:59 +0100

    Added some extra tests for array creations

--- test/idempotency/C005_ArrayInitialization.java
+++ test/idempotency/C005_ArrayInitialization.java
@@ -1,6 +1,13 @@
+import java.util.List; 
+
 class C005_ArrayInitialization {
     {
         String[] names = {"Reinier", "Roel"};
+        String[] names2 = new String[]{"Reinier", "Roel"};
+        String[] names3 = new java.lang.String[]{"Reinier", "Roel"};
+        List[] list1 = new List[0];
+        List<String>[] list2 = new List<String>[0];
+        List<String>[] list3 = new java.util.List<String>[0];
         int[] sized = new int[0];
         int[][] sizedTwoDimensions = new int[0][0];
         int[][] sizedTwoDimensions2 = new int[0][];
