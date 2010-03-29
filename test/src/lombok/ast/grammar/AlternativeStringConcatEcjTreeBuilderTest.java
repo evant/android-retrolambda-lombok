45c484ee4ad1c50c90a0fb9c9f9e62a82a471964
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-03-29 06:28:45 +0200

    The concat tree builder was no longer running on only the relevant file, but on everything. fixed that.

--- test/src/lombok/ast/grammar/AlternativeStringConcatEcjTreeBuilderTest.java
+++ test/src/lombok/ast/grammar/AlternativeStringConcatEcjTreeBuilderTest.java
@@ -1,6 +1,12 @@
 package lombok.ast.grammar;
 
+import static java.util.Collections.singleton;
+
 import java.io.File;
+import java.util.Collection;
+import java.util.regex.Pattern;
+
+import lombok.ast.grammar.RunForEachFileInDirRunner.DirDescriptor;
 
 import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 import org.junit.Test;
@@ -8,8 +14,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(RunForEachFileInDirRunner.class)
 public class AlternativeStringConcatEcjTreeBuilderTest extends EcjTreeBuilderTest {
-	protected boolean shouldProcess(File file) {
-		return file.getName().endsWith("StringConcatExpressions.java");
+	@Override protected Collection<DirDescriptor> getDirDescriptors() {
+		return singleton(DirDescriptor.of(new File("test/resources/idempotency"), true).withInclusion(Pattern.compile("^.*StringConcatExpressions.java$", Pattern.CASE_INSENSITIVE)));
 	}
 	
 	@Test
