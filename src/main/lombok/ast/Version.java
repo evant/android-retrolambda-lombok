b19e820b96d94fc065072d044eacde838be0ea0f
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-05-02 13:31:18 +0200

    Added a version class.

---
+++ src/main/lombok/ast/Version.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2010 Reinier Zwitserloot and Roel Spilker.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package lombok.ast;
+
+/**
+ * This class just holds lombok.ast's current version.
+ */
+public class Version {
+	// ** CAREFUL ** - this class must always compile with 0 dependencies (it must not refer to any other sources or libraries).
+	private static final String VERSION = "0.1-HEAD";
+	
+	private Version() {
+		//Prevent instantiation
+	}
+	
+	/**
+	 * Prints the version followed by a newline, and exits.
+	 */
+	public static void main(String[] args) {
+		System.out.println(VERSION);
+	}
+	
+	/**
+	 * Get the current lombok.ast version.
+	 */
+	public static String getVersion() {
+		return VERSION;
+	}
+}
