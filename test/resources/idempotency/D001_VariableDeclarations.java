396b00a9d669a76ba7eaa372753d49a2ff75d03c
Reinier Zwitserloot <reinier@zwitserloot.com>
2010-10-05 00:29:41 +0200

    Spiced up the Variable declarations tests a bit, especially with more arrays in multiple variables per declaration declarations.
    Co-Author: RJ, Roel

--- test/resources/idempotency/D001_VariableDeclarations.java
+++ test/resources/idempotency/D001_VariableDeclarations.java
@@ -1,8 +1,14 @@
 class D001_VariableDeclarations {
     {
         boolean c;
+        boolean[] c1;
+        boolean c2[];
         String[][] allNames = {{"Reinier", "Roel"}};
         String x, y;
         final int a = 10, b[] = {20};
+        java.lang.String[] f, g[], h[][], i, j[], k;
+        int m, n[], o;
+        int[] p[], q, r[];
+        boolean s[], t[];
     }
 }
