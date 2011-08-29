adc9705ca659bf4d246a63aefbbc0bcc5424a885
Reinier Zwitserloot <reinier@zwitserloot.com>
2011-08-29 21:41:33 +0200

    Added jarjar

---
+++ src/main/lombok/ast/app/Main.java
@@ -0,0 +1,9 @@
+package lombok.ast.app;
+
+import lombok.ast.Version;
+
+public class Main {
+	public static void main(String[] args) {
+		System.err.println("Placeholder - " + Version.getVersion());
+	}
+}
