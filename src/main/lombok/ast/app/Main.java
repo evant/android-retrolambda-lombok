822c10d8b884b4393779c3f18f0ec44802a4978e
Reinier Zwitserloot <reinier@zwitserloot.com>
2011-08-29 21:46:24 +0200

    Made 'Main' actually do something (show a very nice source viewer)

--- src/main/lombok/ast/app/Main.java
+++ src/main/lombok/ast/app/Main.java
@@ -3,7 +3,13 @@ package lombok.ast.app;
 import lombok.ast.Version;
 
 public class Main {
-	public static void main(String[] args) {
-		System.err.println("Placeholder - " + Version.getVersion());
+	public static void main(String[] args) throws Exception {
+		if (args.length > 0 && args[0].equals("--version")) {
+			System.out.println(Version.getVersion());
+			System.exit(0);
+			return;
+		}
+		
+		lombok.ast.printer.Main.main(args);
 	}
 }
