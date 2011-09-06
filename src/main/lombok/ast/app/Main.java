295e961249c0e7d97d55f6ba49c81900cf30e61b
Reinier Zwitserloot <reinier@zwitserloot.com>
2011-09-06 14:48:10 +0200

    lombok.ast now has an extensive 'app' mode where you can for now convert entire (recursive) directories via a 'plan', i.e. ecj,lombok,ecj,text will parse with ecj, convert to lombok.ast, convert back to ecj, then output the source node structure.

--- src/main/lombok/ast/app/Main.java
+++ src/main/lombok/ast/app/Main.java
@@ -1,9 +1,33 @@
 package lombok.ast.app;
 
-import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.tools.SimpleJavaFileObject;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import lombok.ast.Node;
+import lombok.ast.Version;
+import lombok.ast.ecj.EcjTreeBuilder;
+import lombok.ast.ecj.EcjTreeConverter;
+import lombok.ast.ecj.EcjTreePrinter;
+import lombok.ast.grammar.ParseProblem;
+import lombok.ast.grammar.Source;
+import lombok.ast.javac.JcTreeBuilder;
+import lombok.ast.javac.JcTreeConverter;
+import lombok.ast.javac.JcTreePrinter;
+import lombok.ast.printer.HtmlFormatter;
+import lombok.ast.printer.SourceFormatter;
+import lombok.ast.printer.SourcePrinter;
+import lombok.ast.printer.StructureFormatter;
+import lombok.ast.printer.TextFormatter;
 
 import org.eclipse.jdt.internal.compiler.CompilationResult;
 import org.eclipse.jdt.internal.compiler.DefaultErrorHandlingPolicies;
@@ -15,8 +39,11 @@ import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 import org.eclipse.jdt.internal.compiler.parser.Parser;
 import org.eclipse.jdt.internal.compiler.problem.DefaultProblemFactory;
 import org.eclipse.jdt.internal.compiler.problem.ProblemReporter;
+import org.parboiled.google.collect.Lists;
 
-import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.main.OptionName;
@@ -31,23 +58,7 @@ import com.zwitserloot.cmdreader.Mandatory;
 import com.zwitserloot.cmdreader.Sequential;
 import com.zwitserloot.cmdreader.Shorthand;
 
-import lombok.ast.Node;
-import lombok.ast.Version;
-import lombok.ast.ecj.EcjTreeBuilder;
-import lombok.ast.ecj.EcjTreeConverter;
-import lombok.ast.ecj.EcjTreePrinter;
-import lombok.ast.grammar.ContentBasedJavaFileObject;
-import lombok.ast.grammar.ParseProblem;
-import lombok.ast.grammar.Source;
-import lombok.ast.javac.JcTreeBuilder;
-import lombok.ast.javac.JcTreeConverter;
-import lombok.ast.javac.JcTreePrinter;
-import lombok.ast.printer.HtmlFormatter;
-import lombok.ast.printer.SourceFormatter;
-import lombok.ast.printer.SourcePrinter;
-import lombok.ast.printer.StructureFormatter;
-import lombok.ast.printer.TextFormatter;
-
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Main {
 	private static class CmdArgs {
 		@Shorthand("v")
@@ -111,13 +122,160 @@ public class Main {
 			System.exit(0);
 			return;
 		}
+		
+		try {
+			Charset charset = args.encoding == null ? Charset.defaultCharset() : Charset.forName(args.encoding);
+			Main main = new Main(charset, args.verbose);
+			main.compile(args.program);
+			if (!args.print) {
+				File targetDir = new File(args.target);
+				if (!targetDir.exists()) targetDir.mkdirs();
+				if (!targetDir.isDirectory()) {
+					System.err.printf("%s is not a directory or cannot be created\n", targetDir.getCanonicalPath());
+					System.exit(1);
+					return;
+				}
+				main.setOutputDir(targetDir);
+			}
+			
+			for (String input : args.input) {
+				main.addToQueue(input);
+			}
+			
+			main.go();
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+			return;
+		}
 	}
 	
-	private final String encoding;
-	
-	private Main(String encoding) {
-		this.encoding = encoding;
+	private void go() throws IOException {
+		for (Plan p : files) {
+			process(p.getFile(), outDir == null ? null : new File(outDir, p.getRelativeName()));
+		}
+		if (errors > 0) {
+			System.err.printf("%d errors\n", errors);
+		}
+		System.exit(errors > 0 ? 2 : 0);
 	}
+	
+	private void setOutputDir(File f) {
+		this.outDir = f;
+	}
+	
+	private void addToQueue(String item) throws IOException {
+		addToQueue0(new File(item), "");
+	}
+	
+	private void addToQueue0(File f, String pathSoFar) throws IOException {
+		pathSoFar += (pathSoFar.isEmpty() ? "" : "/") + f.getName();
+		if (f.isFile()) {
+			if (f.getName().endsWith(".java")) {
+				files.add(new Plan(f, pathSoFar));
+			}
+		} else if (f.isDirectory()) {
+			for (File inner : f.listFiles()) {
+				addToQueue0(inner, pathSoFar);
+			}
+		} else {
+			throw new IllegalArgumentException("Unknown file: " + f.getCanonicalPath());
+		}
+	}
+	
+	@Data
+	private static class Plan {
+		final File file;
+		final String relativeName;
+	}
+	
+	private void process(File in, File out) throws IOException {
+		if (verbose) {
+			System.out.printf("Processing: %s to %s\n", in.getCanonicalPath(), out == null ? "sysout" : out.getCanonicalPath());
+		}
+		
+		Source source = new Source(Files.toString(in, charset), in.getCanonicalPath());
+		Object transfer = null;
+		try {
+			for (Operation<Object, Object> programElem : program) {
+				transfer = programElem.process(source, transfer);
+			}
+			
+			if (out == null) {
+				System.out.println(transfer);
+			} else {
+				out.getParentFile().mkdirs();
+				Files.write(transfer.toString(), out, charset);
+			}
+		} catch (ConversionProblem cp) {
+			System.err.printf("Can't convert: %s due to %s\n", in.getCanonicalPath(), cp.getMessage());
+			errors++;
+		} catch (RuntimeException e) {
+			System.err.printf("Error during convert: %s\n%s\n", in.getCanonicalPath(), printEx(e));
+			errors++;
+		}
+	}
+	
+	private static String printEx(Throwable t) {
+		val sb = new StringBuilder();
+		sb.append(t.toString());
+		sb.append("\n");
+		Joiner.on("\n").appendTo(sb, t.getStackTrace());
+		return sb.toString();
+	}
+	
+	private void compile(String program) {
+		this.program = compile0(program);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<Operation<Object, Object>> compile0(String program) {
+		String[] parts = program.split("\\s*,\\s*");
+		List<Operation<Object, Object>> out = Lists.newArrayList();
+		if (parts.length == 0) throw new IllegalArgumentException("No operations");
+		Operation<?, ?> initialOp = CONVERSIONS.get("_," + parts[0]);
+		if (initialOp == null) {
+			List<String> initialOps = Lists.newArrayList();
+			for (String key : CONVERSIONS.keySet()) {
+				if (key.startsWith("_,")) initialOps.add(key.substring(2));
+			}
+			throw new IllegalArgumentException(String.format(
+					"Illegal initial operation: %s\nLegal initial operations: %s",
+					parts[0], Joiner.on(",").join(initialOps)));
+		}
+		
+		out.add((Operation<Object, Object>) initialOp);
+		for (int i = 0; i < parts.length - 1; i++) {
+			String convKey = String.format("%s,%s", parts[i], parts[i + 1]);
+			Operation<?, ?> convOp = CONVERSIONS.get(convKey);
+			if (convOp == null) {
+				List<String> convOps = Lists.newArrayList();
+				for (String key : CONVERSIONS.keySet()) {
+					if (key.startsWith(parts[i] + ",")) convOps.add(key.substring(parts[i].length() + 1));
+				}
+				throw new IllegalArgumentException(String.format(
+						"Illegal conversion operation: %s\nLegal conversion operations from %s: %s",
+						convKey, parts[i], Joiner.on(",").join(convOps)));
+			}
+			out.add((Operation<Object, Object>) convOp);
+		}
+		
+		String lastPart = parts[parts.length - 1];
+		if (!LEGAL_FINAL.contains(lastPart)) {
+			throw new IllegalArgumentException(String.format(
+					"Illegal final operation: %s\nLegal final operations: %s",
+					lastPart, Joiner.on(",").join(LEGAL_FINAL)));
+		}
+		
+		return out;
+	}
+	
+	private final Charset charset;
+	private List<Operation<Object, Object>> program;
+	private final boolean verbose;
+	private int errors;
+	private File outDir = null;
+	private final List<Plan> files = Lists.newArrayList();
 	
 	private interface Operation<A, B> {
 		B process(Source source, A in) throws ConversionProblem;
@@ -158,7 +316,7 @@ public class Main {
 					new DefaultProblemFactory()
 				), compilerOptions.parseLiteralExpressionsAsConstants);
 			parser.javadocParser.checkDocComment = true;
-			CompilationUnit sourceUnit = new CompilationUnit(in.getRawInput().toCharArray(), in.getName(), encoding);
+			CompilationUnit sourceUnit = new CompilationUnit(in.getRawInput().toCharArray(), in.getName(), charset.name());
 			CompilationResult compilationResult = new CompilationResult(sourceUnit, 0, 0, 0);
 			CompilationUnitDeclaration cud = parser.parse(sourceUnit, compilationResult);
 			
@@ -174,7 +332,7 @@ public class Main {
 		@Override public JCCompilationUnit process(Source in, Void irrelevant) throws ConversionProblem {
 			Context context = new Context();
 			
-			Options.instance(context).put(OptionName.ENCODING, encoding);
+			Options.instance(context).put(OptionName.ENCODING, charset.name());
 			
 			JavaCompiler compiler = new JavaCompiler(context);
 			compiler.genEndPos = true;
@@ -276,4 +434,35 @@ public class Main {
 			return printer.toString();
 		}
 	};
+	
+	private final Map<String, Operation<?, ?>> CONVERSIONS = ImmutableMap.<String, Operation<?, ?>>builder()
+			.put("_,ecj", parseWithEcj)
+			.put("_,lombok", parseWithLombok)
+			.put("_,javac", parseWithJavac)
+			.put("javac,lombok", javacToLombok)
+			.put("lombok,javac", lombokToJavac)
+			.put("ecj,lombok", ecjToLombok)
+			.put("lombok,ecj", lombokToEcj)
+			.put("lombok,text", lombokToText)
+			.put("lombok,source", lombokToSource)
+			.put("lombok,html", lombokToHtml)
+			.put("ecj,text", ecjToText)
+			.put("javac,text", javacToText)
+			.build();
+	
+	private final List<String> LEGAL_FINAL = ImmutableList.of("source", "html", "text");
+	
+	private static class ContentBasedJavaFileObject extends SimpleJavaFileObject {
+		private final String content;
+		
+		public ContentBasedJavaFileObject(String name, String content) {
+			super(new File(name).toURI(), Kind.SOURCE);
+			this.content = content;
+		}
+		
+		@Override
+		public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+			return content;
+		}
+	}
 }
