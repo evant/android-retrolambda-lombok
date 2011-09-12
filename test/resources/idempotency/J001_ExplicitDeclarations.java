0a62ccdeb67ca24075c9e24c51cc17f4fd9d5c22
Reinier Zwitserloot <reinier@zwitserloot.com>
2011-09-12 22:03:49 +0200

    test file that Roel and Robbert Jan wrote was broken. Fixed it.

--- test/resources/idempotency/J001_ExplicitDeclarations.java
+++ test/resources/idempotency/J001_ExplicitDeclarations.java
@@ -2,30 +2,37 @@ class J001_ExplicitDeclarations {
     public void method0() {
         int x;
     }
+    
     public void method1() {
         int x, y;
     }
+    
     public void method2() {
         class Local {
         }
     }
+    
     public void method3() {
         int x, y;
         class Local {
         }
     }
+    
     public void method4() {
         new Object() {
         }.toString();
     }
+    
     public void method5() {
         for (int x = 0; x < 12; x++) {
         }
     }
+    
     public void method6() {
         for (int x = 0, y = 2; x < 12; x++) {
         }
     }
+    
     public void method7() {
         for (int x : new int[] {1, 2}) {
         }
