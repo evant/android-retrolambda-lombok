df3fbf80b9ac3efe67a3b1d2713a0de30b6a74d5
Reinier Zwitserloot <reinier@zwitserloot.com>
2011-12-22 04:22:44 +0100

    pre-release version bump.

--- src/main/lombok/ast/Version.java
+++ src/main/lombok/ast/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Project Lombok Authors.
+ * Copyright (C) 2010-2011 The Project Lombok Authors.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ package lombok.ast;
  */
 public class Version {
 	// ** CAREFUL ** - this class must always compile with 0 dependencies (it must not refer to any other sources or libraries).
-	private static final String VERSION = "0.1-HEAD";
+	private static final String VERSION = "0.2";
 	
 	private Version() {
 		//Prevent instantiation
